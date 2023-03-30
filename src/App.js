5080615bafc2aa06b17e22991aba8fb29388d4fd
ErenCrt <e.curtseit-2021@hull.ac.uk>
2023-03-30 22:13:39 +0100

    small fixing

--- src/App.js
+++ src/App.js
@@ -5,35 +5,15 @@ import {Register} from "./Register"
 import {Guest} from './Guest';
 import logo from './logo transparent.png';
 import Mainpage from './Mainpage';
-import csv from "./dumbdata.csv"; 
-import Papa from "papaparse"; 
 
 
 function App() {
   const[currentForm, setCurrentForm] = useState('login');
-  const [data, setData] = useState('');
+
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
-
-  const handleData = (results) => {
-    const column1 = [];
-    const column2 = [];
-    const column3 = [];
-    results.data.forEach((row) => {
-      column1.push(row[0]);
-      column2.push(row[1]);
-      column3.push(row[2]);
-    });
-    setData({ column1, column2, column3 }); 
-  };
-
-  // Use PapaParse to read and parse the CSV file when the component mounts
-  Papa.parse(csv, {
-    header: true,
-    complete: handleData,
-  });
 
   return (
     
